60a35c33f7694988c3d83a83c5b4bd7b66c10c1c
Nigel Morse <musmuris@users.noreply.github.com>
2023-01-02 18:56:14 +0000

    show enemy

--- main.py
+++ main.py
@@ -18,21 +18,31 @@ def anchor_center_image(imageFile):
     return image
 
 player_image = anchor_center_image("playerShip2_orange.png")
-player = pyglet.sprite.Sprite(img=player_image, x=window.width/2, y=player_image.height)
+player = pyglet.sprite.Sprite(img=player_image, x=window.width // 2, y=player_image.height)
+
+enemy_image = anchor_center_image("enemyBlue1.png")
 
 laser_image = anchor_center_image("laserRed15.png")
 
 lasers = []
 laserbatch = pyglet.graphics.Batch()
 
+enemies = []
+enemybatch = pyglet.graphics.Batch()
+
 def shoot():
     laser = pyglet.sprite.Sprite(img=laser_image, x=player.x, y=player.y+10, batch=laserbatch)
     lasers.append(laser)
+
+def spawnEnemy():
+    enemy = pyglet.sprite.Sprite(img=enemy_image, x=window.width // 2, y=700, batch=enemybatch)
+    enemies.append(enemy)
 
 @window.event
 def on_draw():
     window.clear()
     laserbatch.draw()
+    enemybatch.draw()
     player.draw()
     
 
@@ -54,14 +64,16 @@ def update(dt):
     for laser in deadlaser:
         lasers.remove(laser)
 
-    if keys[key.LEFT] and player.x > player.width / 2 + 10:
+    if keys[key.LEFT] and player.x > player.width // 2 + 10:
             player.x -= dt * PLAYER_SPEED
-    if keys[key.RIGHT] and player.x < window.width - player.width / 2 - 10:
+    if keys[key.RIGHT] and player.x < window.width - player.width // 2 - 10:
             player.x += dt * PLAYER_SPEED
     if keys[key.SPACE] and (timeSoFar - lastLaserTime) > LASER_COOLDOWN:
         lastLaserTime = timeSoFar
         shoot()
 
+    if len(enemies) == 0 :
+        spawnEnemy()
 
 
 pyglet.clock.schedule_interval(update, 1/60)
