ab4b254d81c2a40e4a294e7d7c1d4b8c1a5a00be
Nigel Morse <musmuris@users.noreply.github.com>
2023-01-02 18:54:48 +0000

    shoot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyglet
 from pyglet.window import key
 
 PLAYER_SPEED = 300.0
+LASER_SPEED = 500.0
 
 window = pyglet.window.Window(1024,768)
 keys = key.KeyStateHandler()
@@ -19,17 +20,48 @@ def anchor_center_image(imageFile):
 player_image = anchor_center_image("playerShip2_orange.png")
 player = pyglet.sprite.Sprite(img=player_image, x=window.width/2, y=player_image.height)
 
+laser_image = anchor_center_image("laserRed15.png")
+
+lasers = []
+laserbatch = pyglet.graphics.Batch()
+
+def shoot():
+    laser = pyglet.sprite.Sprite(img=laser_image, x=player.x, y=player.y+10, batch=laserbatch)
+    lasers.append(laser)
 
 @window.event
 def on_draw():
     window.clear()
+    laserbatch.draw()
     player.draw()
+    
+
+LASER_COOLDOWN = 0.5
+timeSoFar = 0
+lastLaserTime = 0 
 
 def update(dt):
+    global timeSoFar, lastLaserTime
+
+    timeSoFar += dt
+
+    deadlaser = []
+    for laser in lasers:
+        laser.y += dt * LASER_SPEED
+        if laser.y > window.height + laser_image.height:
+            laser.delete()
+            deadlaser.append(laser)
+    for laser in deadlaser:
+        lasers.remove(laser)
+
     if keys[key.LEFT] and player.x > player.width / 2 + 10:
             player.x -= dt * PLAYER_SPEED
     if keys[key.RIGHT] and player.x < window.width - player.width / 2 - 10:
             player.x += dt * PLAYER_SPEED
+    if keys[key.SPACE] and (timeSoFar - lastLaserTime) > LASER_COOLDOWN:
+        lastLaserTime = timeSoFar
+        shoot()
+
 
 
 pyglet.clock.schedule_interval(update, 1/60)
