75f6a59ae118baf3007bfdb605d37813265b6469
Nigel Morse <musmuris@users.noreply.github.com>
2023-01-02 18:52:10 +0000

    move ship

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pyglet
+from pyglet.window import key
+
+PLAYER_SPEED = 300.0
 
 window = pyglet.window.Window(1024,768)
-keys = pyglet.window.key.KeyStateHandler()
+keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
 pyglet.resource.path = ['assets']
@@ -14,13 +17,22 @@ def anchor_center_image(imageFile):
     return image
 
 player_image = anchor_center_image("playerShip2_orange.png")
-player_sprite = pyglet.sprite.Sprite(img=player_image, x=window.width/2, y=player_image.height)
+player = pyglet.sprite.Sprite(img=player_image, x=window.width/2, y=player_image.height)
 
 
 @window.event
 def on_draw():
     window.clear()
-    player_sprite.draw()
+    player.draw()
+
+def update(dt):
+    if keys[key.LEFT] and player.x > player.width / 2 + 10:
+            player.x -= dt * PLAYER_SPEED
+    if keys[key.RIGHT] and player.x < window.width - player.width / 2 - 10:
+            player.x += dt * PLAYER_SPEED
+
+
+pyglet.clock.schedule_interval(update, 1/60)
 
 pyglet.app.run()
 
